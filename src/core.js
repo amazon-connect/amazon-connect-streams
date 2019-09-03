35922cf5d69d811d403297260ea72561a633da0b
Shuchun Yang <s.yang@salesforce.com>
2019-09-03 10:23:13 -0700

    fix loginWindow typo

--- src/core.js
+++ src/core.js
@@ -421,7 +421,7 @@
             try {
                var loginUrl = createLoginUrl(params);
                connect.getLog().warn("ACK_TIMEOUT occurred, attempting to pop the login page if not already open.");
-               connect.core.loginWinow = connect.core.getPopupManager().open(loginUrl, connect.MasterTopics.LOGIN_POPUP);
+               connect.core.loginWindow = connect.core.getPopupManager().open(loginUrl, connect.MasterTopics.LOGIN_POPUP);
 
             } catch (e) {
                connect.getLog().error("ACK_TIMEOUT occurred but we are unable to open the login popup.").withException(e);
@@ -438,9 +438,9 @@
                global.clearInterval(connect.core.iframeRefreshInterval);
                connect.core.iframeRefreshInterval = null;
                connect.core.getPopupManager().clear(connect.MasterTopics.LOGIN_POPUP);
-               if (params.loginPopupAutoClose && connect.core.loginWinow) {
-                  connect.core.loginWinow.close();
-                  connect.core.loginWinow = null;
+               if (params.loginPopupAutoClose && connect.core.loginWindow) {
+                  connect.core.loginWindow.close();
+                  connect.core.loginWindow = null;
                }
             });
          }
