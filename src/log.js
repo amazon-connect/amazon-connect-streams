a12c5d7bd0e52773a63af1aa079bee37f7d8f120
Andrew Fairbairn <AndrewFairbairn@users.noreply.github.com>
2019-11-22 13:06:46 +0100

    - Optional parameter to name log file output

--- src/log.js
+++ src/log.js
@@ -383,11 +383,11 @@
       return lines.join("\n");
    };
 
-   Logger.prototype.download = function() {
+   Logger.prototype.download = function(logName = 'agent-log') {
       var logBlob = new global.Blob([JSON.stringify(this._rolledLogs.concat(this._logs), undefined, 4)], ['text/plain']);
       var downloadLink = document.createElement('a');
       downloadLink.href = global.URL.createObjectURL(logBlob);
-      downloadLink.download = 'agent-log.txt';
+      downloadLink.download = logName + '.txt';
       document.body.appendChild(downloadLink);
       downloadLink.click();
       document.body.removeChild(downloadLink);
