28f93b317b7b9c11e7f757861890de9046fdb141
Andrew Fairbairn <AndrewFairbairn@users.noreply.github.com>
2019-11-22 13:15:31 +0100

    - Add optional log file name when downloading logs

--- src/log.js
+++ src/log.js
@@ -383,15 +383,15 @@
       return lines.join("\n");
    };
 
-   Logger.prototype.download = function() {
-      var logBlob = new global.Blob([JSON.stringify(this._rolledLogs.concat(this._logs), undefined, 4)], ['text/plain']);
-      var downloadLink = document.createElement('a');
-      downloadLink.href = global.URL.createObjectURL(logBlob);
-      downloadLink.download = 'agent-log.txt';
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-      document.body.removeChild(downloadLink);
-   };
+  Logger.prototype.download = function(logName = 'agent-log') {
+    var logBlob = new global.Blob([JSON.stringify(this._rolledLogs.concat(this._logs), undefined, 4)], ['text/plain']);
+    var downloadLink = document.createElement('a');
+    downloadLink.href = global.URL.createObjectURL(logBlob);
+    downloadLink.download = logName + '.txt';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
 
    Logger.prototype.scheduleUpstreamLogPush = function(conduit) {
       if (!connect.upstreamLogPushScheduled) {
