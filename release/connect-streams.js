0150a78a6c02e668d8b179a50d6be668c96645f9
Cyrus Twombly <ctwombly@amazon.com>
2019-12-20 21:29:02 +0000

    Revert "updated release files for completeContact revert"
    This reverts commit 6aa69c7cfd4886be24f44147c08f44c240b86211.

--- release/connect-streams.js
+++ release/connect-streams.js
@@ -22778,6 +22778,7 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
          'updateAgentConfiguration',
          'acceptContact',
          'createOutboundContact',
+         'completeContact',
          'destroyContact',
          'notifyContactIssue',
          'updateContactAttributes',
@@ -22925,10 +22926,6 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
       var self = this;
       var log = connect.getLog();
 
-      params.authentication = {
-         authToken: this.authToken
-      };
-
       if (! connect.contains(this.client, method)) {
          var message = connect.sprintf('No such method exists on AWS client: %s', method);
          callbacks.failure(new connect.ValueError(message), {message: message});
@@ -22974,6 +22971,10 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
       }
    };
 
+   AWSClient.prototype._requiresAuthenticationParam = function(method) {
+      return method !== connect.ClientMethods.COMPLETE_CONTACT;
+   };
+
    AWSClient.prototype._translateParams = function(method, params) {
       switch (method) {
          case connect.ClientMethods.UPDATE_AGENT_CONFIGURATION:
@@ -22991,6 +22992,12 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
 
          default:
             break;
+      }
+
+      if (this._requiresAuthenticationParam(method)) {
+         params.authentication = {
+            authToken: this.authToken
+         };
       }
 
       return params;
@@ -23873,6 +23880,13 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
     }, callbacks);
   };
 
+  Contact.prototype.complete = function (callbacks) {
+    var client = connect.core.getClient();
+    client.call(connect.ClientMethods.COMPLETE_CONTACT, {
+      contactId: this.getContactId()
+    }, callbacks);
+  };
+
   Contact.prototype.notifyIssue = function (issueCode, description, callbacks) {
     var client = connect.core.getClient();
     client.call(connect.ClientMethods.NOTIFY_CONTACT_ISSUE, {
@@ -24037,10 +24051,8 @@ AWS.apiLoader.services['sts']['2011-06-15'] = require('../apis/sts-2011-06-15.mi
     return this._getData().monitoringInfo;
   };
 
-
   Connection.prototype.destroy = function (callbacks) {
-    var client = connect.core.getClient(), self = this;
-
+    var client = connect.core.getClient();
     client.call(connect.ClientMethods.DESTROY_CONNECTION, {
       contactId: this.getContactId(),
       connectionId: this.getConnectionId()
