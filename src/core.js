1ceecf85533cbf4103215da3f1de79ed1672ce8e
Yingdong Li <yingdoli@amazon.com>
2020-02-06 17:23:10 -0500

    allow adding callbacks for onViewContact handler in initCCP()

--- src/core.js
+++ src/core.js
@@ -518,6 +518,10 @@
         });
       }
     });
+
+    if (params.onViewContact) {
+  		connect.core.onViewContact(params.onViewContact);
+  	}
   };
 
   /**-----------------------------------------------------------------------*/
