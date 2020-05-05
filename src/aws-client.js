0240f6b1c7aa8475a1be437adc732d14078b6df1
Anton Stang <astang@tecracer.de>
2020-05-05 14:29:18 +0200

    fix(aws-client): add missing setter function to avoid type error when importing streams from node modules

--- src/aws-client.js
+++ src/aws-client.js
@@ -2733,15 +2733,18 @@ require('../lib/node_loader');
 var AWS = require('../lib/core');
 var Service = AWS.Service;
 var apiLoader = AWS.apiLoader;
+var model = require('../apis/sts-2011-06-15.min.json');
 
 apiLoader.services['sts'] = {};
 AWS.STS = Service.defineService('sts', ['2011-06-15']);
 require('../lib/services/sts');
 Object.defineProperty(apiLoader.services['sts'], '2011-06-15', {
   get: function get() {
-    var model = require('../apis/sts-2011-06-15.min.json');
     model.paginators = require('../apis/sts-2011-06-15.paginators.json').pagination;
     return model;
+  },
+  set: function(value) {
+    model = value;
   },
   enumerable: true,
   configurable: true
