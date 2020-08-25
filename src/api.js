0691c4c2d8877c8aed17ca3853d9115910d8ef81
John Lim <jhlim@amazon.com>
2020-08-25 02:31:14 -0400

    Add agent nextState API
    cr https://code.amazon.com/reviews/CR-32546665

--- src/api.js
+++ src/api.js
@@ -293,6 +293,10 @@
     return this._getData().snapshot.state;
   };
 
+  Agent.prototype.getNextState = function () {
+    return this._getData().snapshot.nextState;
+  };
+
   Agent.prototype.getAvailabilityState = function () {
     return this._getData().snapshot.agentAvailabilityState;
   };
@@ -381,11 +385,17 @@
       });
   };
 
-  Agent.prototype.setState = function (state, callbacks) {
+  Agent.prototype.setState = function (state, callbacks, options) {
     var client = connect.core.getClient();
     client.call(connect.ClientMethods.PUT_AGENT_STATE, {
-      state: connect.assertNotNull(state, 'state')
+      state: connect.assertNotNull(state, 'state'),
+      enqueueNextState: options && !!options.enqueueNextState
     }, callbacks);
+  };
+
+  Agent.prototype.onEnqueuedNextState = function (f) {
+    var bus = connect.core.getEventBus();
+    bus.subscribe(connect.AgentEvents.ENQUEUED_NEXT_STATE, f);
   };
 
   Agent.prototype.setStatus = Agent.prototype.setState;
--- src/index.d.ts
+++ src/index.d.ts
@@ -412,6 +412,11 @@ declare namespace connect {
     readonly queueARN?: string;
   }
 
+  interface AgentSetStateOptions {
+    /**  Enables enqueuing agent state while agent is handling a live contact. */
+    readonly enqueueNextState?: boolean;
+  }
+
   /**
    * The Agent API provides event subscription methods and action methods which can be called on behalf of the agent.
    * There is only ever one agent per Streams instantiation and all contacts and actions are assumed to be taken on behalf of this one agent.
@@ -567,12 +572,17 @@ declare namespace connect {
 
     /**
      * Set the agent's current availability state.
-     * Can only be performed if the agent is not handling a live contact.
+     * Will enqueue state if the agent is handling a live contact and enqueueNextState is true.
      *
      * @param state The new agent state.
      * @param callbacks Success and failure callbacks to determine whether the operation was successful.
+     * @param options
      */
-    setState(state: AgentStateDefinition, callbacks?: SuccessFailOptions): void;
+    setState(
+      state: AgentStateDefinition,
+      callbacks?: SuccessFailOptions,
+      options?: AgentSetStateOptions
+    ): void;
 
     /** Alias for `setState()`. */
     setStatus(
@@ -620,6 +630,13 @@ declare namespace connect {
      * @param callback A callback to receive updates on agent mute state
      */
     onMuteToggle(callback: AgentMutedStatusCallback): void;
+
+    /**
+     * Subscribe a method to be called when the agent has a nextState.
+     *
+     * @param callback A callback that is invoked with the Agent object.
+     */
+    onEnqueuedNextState(callback: Agent): void;
   }
 
   interface AgentMutedStatus {
