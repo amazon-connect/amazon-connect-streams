a368c2f782a6109b3fb985f3ea9c3344fc971f96
zesheng <zesheng@amazon.com>
2020-09-03 10:17:56 -0400

    add idempotencyToken and remove endpointId
    cr https://code.amazon.com/reviews/CR-33280168

--- src/api.js
+++ src/api.js
@@ -407,10 +407,15 @@
 
   Agent.prototype.createTask = function(taskContact, callbacks) {
     connect.assertNotNull(taskContact, 'Task contact object');
-    
+    connect.assertNotNull(taskContact.name, 'Task name');
+    connect.assertNotNull(taskContact.endpoint, 'Task endpoint');
+
+    taskContact.idempotencyToken = AWS.util.uuid.v4();
+    delete taskContact.endpoint.endpointId;
+
     var client = connect.core.getClient();
 
-    client.call(connect.ClientMethods.CREATE_TASK_CONTACT, taskContact,callbacks);
+    client.call(connect.ClientMethods.CREATE_TASK_CONTACT, taskContact, callbacks);
   };
 
   Agent.prototype.getAllQueueARNs = function () {
