7bdb2da0e9ba3564f45694fb5028b23e3554e793
Cyrus Twombly <ctwombly@amazon.com>
2021-05-18 15:35:08 -0400

    bump to 1.6.8

--- release/connect-streams.js
+++ release/connect-streams.js
@@ -25489,7 +25489,7 @@
 
   connect.core = {};
   connect.core.initialized = false;
-  connect.version = "1.6.7";
+  connect.version = "1.6.8";
   connect.DEFAULT_BATCH_SIZE = 500;
  
   var CCP_SYN_TIMEOUT = 1000; // 1 sec
