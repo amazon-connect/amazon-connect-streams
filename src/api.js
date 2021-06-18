180b3699a04bff507d840e9e2a9bfa06b74fce76
zesheng <zesheng@amazon.com>
2021-06-18 10:42:44 -0400

    Bug fix when there is no customer id
    cr: https://code.amazon.com/reviews/CR-52289494

--- src/api.js
+++ src/api.js
@@ -1189,7 +1189,7 @@
               }
               resolve(obj);
             } else {
-              var error = connect.VoiceIdError(connect.VoiceIdErrorTypes.NO_SPEAKER_ID_FOUND, "No speakerId assotiated with this call", err);
+              var error = connect.VoiceIdError(connect.VoiceIdErrorTypes.NO_SPEAKER_ID_FOUND, "No speakerId assotiated with this call");
               reject(error);
             }
             
